eb08ce1a22f2cb6db17ef0dc55dd2242933428cf
icnpdesign <inp@bu.edu>
2017-11-07 02:04:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,5 +23,5 @@ function calculatesFarePrice (startBlock, endBlock) {
     price = 0;
   }
        return price;
-       default: 'cannot travel that far';
+       default: return 'cannot travel that far';
     }
