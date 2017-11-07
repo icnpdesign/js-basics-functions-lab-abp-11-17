0968677a44ac0cd417db1e7183fab8df889ce181
icnpdesign <inp@bu.edu>
2017-11-07 00:04:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+  const price = getPrice(pickupLocation);
+  
+  if (price > 42) {
+  console.log(pickupLocation - 42 "blocks");
+} else {
+  console.log(42 - pickupLocation "blocks");
+}
