28d9ef280347630dd5b4773b5434134cf1d8b012
icnpdesign <inp@bu.edu>
2017-11-07 01:11:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
-function distanceFromHqInBlocks (start) {
+function distanceFromHqInBlocks (start, end) {
   if (start > end) {
   const distance = (start - end);
   return distance;
-} else { const distance = (42 - end);
+} else { const distance = (end - start);
 return distance; }
 }
