d20c8ecb5a5af4c64b992085084cb5953c1c416f
icnpdesign <inp@bu.edu>
2017-11-07 00:13:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(pickupLocation) {
-  let pickupLocation;
-
-  if (pickupLocation > 42) {
-  console.log(pickupLocation - 42 "blocks");
-} else {
-  console.log(42 - pickupLocation "blocks");
+function calculateVertical (pickupLocation); {
+  (pickupLocation - scuberHQ) * 4
 }
 
-distanceFromHqInBlocks (43);
