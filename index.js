c8c1ff518860b8aaab1bd1881a09af0c0196e1ed
icnpdesign <inp@bu.edu>
2017-11-07 00:10:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(pickupLocation) {
   let pickupLocation;
-  
+
   if (pickupLocation > 42) {
   console.log(pickupLocation - 42 "blocks");
 } else {
